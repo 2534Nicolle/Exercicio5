faf378c1a39818e7fcdf1f9c558d4503b354652d
2534Nicolle <162452766+2534Nicolle@users.noreply.github.com>
2025-04-15 12:26:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function aumentarFonte() {
     const paragrafo = document.getElementById("paragrafo");
     let tamanhoFonte = window.getComputedStyle(paragrafo).fontSize;
-    tamanhoFonte = parseInt(tamanhoFonte); // Converte o valor para número inteiro
+    tamanhoFonte = parseInt(tamanhoFonte); 
   
-    paragrafo.style.fontSize = (tamanhoFonte + 2) + "px"; // Aumenta o tamanho da fonte em 2px
+    paragrafo.style.fontSize = (tamanhoFonte + 2) + "px";
   }
   
